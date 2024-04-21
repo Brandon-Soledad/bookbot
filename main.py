ffe78e19e45bea6847649c500807986965dee9a0
soledb25 <soledb25@gmail.com>
2024-04-21 13:52:39 -0700

    counting words in book

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
     path = "books/frankenstein.txt"
+    text = get_text(path)
+    num_words = len(text.split())
+    print(f"The frankenstein book has {num_words} words!")
+
+def get_text(path):
     with open(path) as f:
-        print(f.read())
+        return f.read()
 
 main()
